2b74a1dbc82a08fa0b9131d8eba0fe5b3633d22c
LunaCI <lunaci@luadist.org>
2018-06-08 02:45:55 +0000

    Update to version 0.1-2

--- riseml-0.1-2.rockspec
+++ riseml-0.1-2.rockspec
@@ -1,16 +1,16 @@
 -- This file was automatically generated for the LuaDist project.
 
 package = "riseml"
-version = "0.1-1"
+version = "0.1-2"
 -- LuaDist source
 source = {
-  tag = "0.1-1",
+  tag = "0.1-2",
   url = "git://github.com/LuaDist-testing/riseml.git"
 }
 -- Original source
 -- source = {
 --    url = "git://github.com/riseml/client-lua",
---    tag = "v0.1-1"
+--    tag = "v0.1-2"
 -- }
 description = {
    summary = "",
--- src/riseml.lua
+++ src/riseml.lua
@@ -65,6 +65,38 @@ function riseml.serve_rectangles(predict_function)
       {"/predict", PredictHandler}
   })
 
+function riseml.serve_it_t(predict_function)
+  local PredictHandler = class("Predict", turbo.web.RequestHandler)
+  function PredictHandler:post()
+    local image = self:get_argument("image", "ERROR")
+    local question = self:get_argument("question", "ERROR")
+    timer = torch.Timer()
+    answer = predict_function(image, question)
+    self:set_status(200)
+    self:add_header('Access-Control-Allow-Origin','*')
+    self:add_header("Content-Type", "image/jpeg")
+    self:write(answer)
+    local time = timer:time().real
+    turbo.log.success(string.format('Request answered in %.4fs', time))
+  end
+
+  function PredictHandler:options()
+    self:add_header('Access-Control-Allow-Methods', 'POST')
+    self:add_header('Access-Control-Allow-Headers', 'content-type')
+    self:add_header('Access-Control-Allow-Origin', '*')
+  end
+
+  print('webserver running...')
+  -- Create an Application object and bind our PredictHandler to the route '/predict'.
+  local app = turbo.web.Application:new({
+      {"/predict", PredictHandler}
+  })
+
+  -- Set the server to listen on port 80 and start the ioloop.
+  app:listen(80)
+  turbo.ioloop.instance():start()
+end
+
   -- Set the server to listen on port 80 and start the ioloop.
   app:listen(80)
   turbo.ioloop.instance():start()
